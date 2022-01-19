bd94ef5747861c8931952753e763efaa8c78ba09
Natascha Haberl <nt.haberl@gmail.com>
2022-01-18 21:26:09 -0800

    updated

---
+++ MorningAlgo/Week2/MergeDedupe.js
@@ -0,0 +1,11 @@
+// given two sorted arrays that may have duplicate values, merge them to create a new sorted array and remove any duplicates
+function mergeDedupe(arr1, arr2) {}
+
+// try out some other tests
+mergeDedupe([1, 3, 3, 5, 8, 10], [1, 3, 4, 5]) // [ 1, 3, 4, 5, 8, 10 ]
+mergeDedupe([2,3,3,5,8,10,12], [1,3,4,6]) // [1, 2, 3, 4, 5, 6, 8, 10, 12]
+
+//      a -> 
+//     [1, 3, 3, 5, 8, 10]
+//      b
+//     [1, 3, 4, 5]
