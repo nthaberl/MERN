8fdb6a370412556cd7f2c8259fba275ef4439daf
Natascha Haberl <nt.haberl@gmail.com>
2022-01-20 21:57:03 -0800

    updated

---
+++ MorningAlgo/Week2/ConsecutiveSums.js
@@ -0,0 +1,27 @@
+// This is an actual interview algorithm given to a Coding Dojo alum
+
+// Find Consecutive Sums
+
+// You are given arr, a list of positive integers 0-255
+// You are given k, a integer between 1-255
+
+// Find all the consecutive groups of integers that add up to the value k
+
+// inputs
+// k = 16
+// arr = [2, 5, 3, 6, 7, 0, 0, 23, 11]
+
+// outputs
+// [
+//   [2, 5, 3, 6],
+//   [3, 6, 7]  // 3, 6, 7 appear consecutively, so they are including in the solution
+//   [3, 6, 7, 0],
+//   [3, 6, 7, 0, 0]
+// ]
+
+// create new arrays
+// if no matches, return empty array
+
+function findConsecutiveSums(arr, k) { }
+
+console.log(findConsecutiveSums([2,5,3,6,7,0,0,23,11], 16));
